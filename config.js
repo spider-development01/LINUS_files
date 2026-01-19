5f32ea25a4cc14e62f16e3b28e03eeaf81d20dcd
Spider-Development <147894555+spider-development01@users.noreply.github.com>
2026-01-19 09:06:39 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+// Locally: Paste your keys here.
+// On Netlify: We will overwrite this file automatically using Build Commands.
+
+window.SUPABASE_URL = 'YOUR_SUPABASE_URL';
+window.SUPABASE_KEY = 'YOUR_SUPABASE_ANON_KEY';
