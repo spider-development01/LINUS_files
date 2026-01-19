8b5bc25b6dd6a2bb28235a61599125a4c42d13a0
Spider-Development <147894555+spider-development01@users.noreply.github.com>
2026-01-19 09:02:22 +0530

    Update cred.js

--- cred.js
+++ cred.js
@@ -1,3 +1,3 @@
-        const SUPABASE_URL = 'process.env.SUPABASE_URL'; // Replace
-        const SUPABASE_KEY = 'process.env.SUPABASE_KEY '; // Replace
-        const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const SUPABASE_URL = 'process.env.SUPABASE_URL'; // Replace
+const SUPABASE_KEY = 'process.env.SUPABASE_KEY '; // Replace
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
